152757fade0297bc0860b13e43c1a71f4b1a72ce
surajdesai1297@gmail.com <suraj@LAPTOP-C83DOH9J.localdomain>
2020-09-21 14:02:23 +0530

    Final Commit

--- ALICE IN WONDERLAND/src/index.js
+++ ALICE IN WONDERLAND/src/index.js
@@ -6,14 +6,19 @@
 // she was confused which is the right way that will lead her to her home. 
 // She found a cute bird, standing in one of the tree. 
 // Alice asked the bird the way to go back to her home.
-// The bird said a two digit number( say 23 ) and asked her to find the sum of its digits (2+3=5) and that numbered way will lead her to her home.
+// The bird said a two digit number( say 23 ) and asked her to find the sum of its digits (2+3=5) and that
+// numbered way will lead her to her home.
 // Alice was already confused, so pls help Alice in finding the route to her home....
 
 
 //Write a function to return the route to her home
-function find_Route(number){
-    
-    return null;
+function find_Route(number) {
+
+    // splitting two digit number into array
+    number = `${number}`.split("");
+
+    // addning tow digit number and returning sum value
+    return parseInt(number[0]) + parseInt(number[1]);
 }
 
 //Run SpecRunner.html to check the Test Case.
--- BASIC OPERATORS/src/index.js
+++ BASIC OPERATORS/src/index.js
@@ -6,11 +6,14 @@
 
 
 //write a function to return the value based on operator.
-function basicOp(operation,value1,value2){
+function basicOp(operation, value1, value2) {
 
+  // if he operation is +,-,/,* and the numbers are integer then only the operation will happen.
+  if ('+-/*'.includes(operation) && typeof value1 == 'number' && typeof value2 == 'number') {
 
-
-  return null;
+    return eval(`${value1} ${operation} ${value2}`)
+  }
+  return 'Invalid Operator';
 }
 
 //Run SpecRunner.html to check the Test Case
